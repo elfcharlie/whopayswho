cf1e1788b3ea67c9f6ded793c8a3e1485b8938aa
elfcharlie <charlie.elf94@gmail.com>
2022-10-22 01:15:41 +0200

    Update Vertical stacking and inputfield data

--- Assets/PersonValues.cs
+++ Assets/PersonValues.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PersonValues : MonoBehaviour
 {
     public string name;
     public float paidAmount;
+    private TMP_InputField nameInput;
+    private TMP_InputField amountInput;
 
+    void Start()
+    {
+        nameInput = gameObject.transform.Find("NameInput").GetComponent<TMP_InputField>();
+        amountInput = gameObject.transform.Find("AmountInput").GetComponent<TMP_InputField>();
+    }
     public void ChangeName()
     {
-        name = this.text;
-        Debug.Log(name);
+        name = nameInput.text;
+    }
+    public void ChangeAmount()
+    {
+        float.TryParse(amountInput.text, out paidAmount);
+        Debug.Log(paidAmount);
     }
 }
